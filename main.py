012fb1dd76937424ae93b9011761542d95b4054d
Kai Chevannes <chevannes.kai@gmail.com>
2023-07-28 17:06:53 +0100

    cleanup main

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-#from spec_parser import SpecParser
 from display import Display
 
 # Test profiles for B2B, B2C, middleground
 
 if __name__ == "__main__":
-    # spec_parser = SpecParser("spec.txt")
-    # spec_parser.parse()
-    # for section in spec_parser.sections:
-    #     print("=============================")
-    #     print(f"Section Category = {section.category}")
-    #     for task in section.tasks:
-    #         print(task)
-    #     print("=============================")
     display = Display()
     display.display("spec.txt")
